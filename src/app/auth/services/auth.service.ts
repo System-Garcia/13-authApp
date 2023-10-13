cbe8481c8b36bf873dd80c2be97f32a751ed374a
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-13 15:15:21 -0700

    Correcion de error: Agregar parentesis a la señanl

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -17,8 +17,8 @@ export class AuthService {
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
   // Para que nadie modifique el valor del user actual usamos una señal computada
-  public currentUser = computed( () => this._currentUser)
-  public authStatus = computed( () => this._authStatus)
+  public currentUser = computed( () => this._currentUser())
+  public authStatus = computed( () => this._authStatus())
 
   constructor() { }
 
