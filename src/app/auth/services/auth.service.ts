d8359e67adb2b662b00b7f241fb9e84cf3deb77e
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-13 14:58:25 -0700

    Conexion al backend mediante el servicio

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +16,32 @@ export class AuthService {
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  // Para que nadie modifique el valor del user actual usamos una señal computada
+  public currentUser = computed( () => this._currentUser)
+  public authStatus = computed( () => this._authStatus)
+
   constructor() { }
 
   login(email: string, password: string): Observable<Boolean> {
+
+    const url = `${this.baseUrl}/auth/login`
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      // Destructuring
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+
+        console.log({ user, token });
+      }),
+      map(() => true),
+
+      // TODO: errores
+      // Este operador nos permite regresar un error sin importar el tipado
+      catchError((err) => throwError(() => err.error.message))
+    );
 
     return of(true)
   }
