b86da77d6cc7e1d4b42717eb7c8bc52117db9509
insideyiu <a22311061@uthermosillo.edu.mx>
2023-10-13 17:03:51 -0700

    Refactorizar el establecimiento de la autenticacion

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -26,22 +26,21 @@ export class AuthService {
 
   constructor() {}
 
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+
+    return true;
+  }
+
   login(email: string, password: string): Observable<Boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
       // Destructuring
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-
-        console.log({ user, token });
-      }),
-      map(() => true),
-
-      // TODO: errores
+      map( ({user, token}) => this.setAuthentication(user, token)),
       // Este operador nos permite regresar un error sin importar el tipado
       catchError((err) => throwError(() => err.error.message))
     );
@@ -59,13 +58,9 @@ export class AuthService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
 
-        return true;
-      }),
+      // Retornar observable con true
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError(() => { // En este ya sabemos que no esta autenticado
         this._authStatus.set( AuthStatus.notAuthenticated);
         return of(false);
